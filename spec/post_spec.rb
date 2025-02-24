199fa25d00810a9eac958a8d219041f6406adc79
Jolyon Pawlyn <jpawlyn@gmail.com>
2025-02-24 16:31:56 +0100

    Refactor test in preparation for testing class methods

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -11,28 +11,32 @@ describe Post do
   let(:tag4) { Tag.create! }
 
   context 'has_many through:' do
-    it 'return posts that have the most authors in common with post' do
-      post = Post.create! authors: [author1, author2, author3]
-      post1 = Post.create! authors: [author1, author4]
-      post2 = Post.create! authors: [author1, author2]
-      post3 = Post.create! authors: [author4]
-      post4 = Post.create! authors: [author1, author2, author3]
+    let!(:post)  { Post.create! authors: [author1, author2, author3] }
+    let!(:post1) { Post.create! authors: [author1, author4] }
+    let!(:post2) { Post.create! authors: [author1, author2] }
+    let!(:post3) { Post.create! authors: [author4] }
+    let!(:post4) { Post.create! authors: [author1, author2, author3] }
 
-      expect(post.similar_posts.map(&:similar_posts_model_count)).to eq([3, 2, 1])
-      expect(post.similar_posts).to eq([post4, post2, post1])
+    describe '#similar_posts' do
+      it 'return posts that have the most authors in common with post' do
+        expect(post.similar_posts.map(&:similar_posts_model_count)).to eq([3, 2, 1])
+        expect(post.similar_posts).to eq([post4, post2, post1])
+      end
     end
   end
 
   context 'has_and_belongs_to_many' do
-    it 'return posts that have the most tags in common with post' do
-      post = Post.create! tags: [tag1, tag2, tag3]
-      post1 = Post.create! tags: [tag1, tag4]
-      post2 = Post.create! tags: [tag1, tag2]
-      post3 = Post.create! tags: [tag4]
-      post4 = Post.create! tags: [tag1, tag2, tag3]
+    let!(:post) { Post.create! tags: [tag1, tag2, tag3] }
+    let!(:post1) { Post.create! tags: [tag1, tag4] }
+    let!(:post2) { Post.create! tags: [tag1, tag2] }
+    let!(:post3) { Post.create! tags: [tag4] }
+    let!(:post4) { Post.create! tags: [tag1, tag2, tag3] }
 
-      expect(post.similar_posts_by_tag.map(&:similar_posts_by_tag_model_count)).to eq([3, 2, 1])
-      expect(post.similar_posts_by_tag).to eq([post4, post2, post1])
+    describe '#similar_posts_by_tag' do
+      it 'return posts that have the most tags in common with post' do
+        expect(post.similar_posts_by_tag.map(&:similar_posts_by_tag_model_count)).to eq([3, 2, 1])
+        expect(post.similar_posts_by_tag).to eq([post4, post2, post1])
+      end
     end
   end
 
@@ -43,9 +47,11 @@ describe Post do
     let!(:post3) { Post.create! tags: [tag4] }
     let!(:post4) { Post.create! authors: [author1], tags: [tag1, tag2, tag3] }
 
-    it 'return posts that have the most authors and tags in common with post' do
-      expect(post.similar_posts_by_author_and_tag.map(&:similar_posts_by_author_and_tag_model_count)).to eq([5, 4, 1])
-      expect(post.similar_posts_by_author_and_tag).to eq([post1, post4, post2])
+    describe '#similar_posts_by_author_and_tag' do
+      it 'return posts that have the most authors and tags in common with post' do
+        expect(post.similar_posts_by_author_and_tag.map(&:similar_posts_by_author_and_tag_model_count)).to eq([5, 4, 1])
+        expect(post.similar_posts_by_author_and_tag).to eq([post1, post4, post2])
+      end
     end
   end
 end
